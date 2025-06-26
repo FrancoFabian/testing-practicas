900eba15f310cdeeb597ea0ab5dae61653f957bd
FrancoFabian <jeralfabian489@gmail.com>
2025-06-25 20:55:51 -0600

    fix: para que se vea cuando se hacen los test de cuccumber

--- src/test/java/com/practicando/demo/runner/CucumberTest.java
+++ src/test/java/com/practicando/demo/runner/CucumberTest.java
@@ -8,14 +8,17 @@ import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+@Suite                           // ← indica que es una test-suite
+@IncludeEngines("cucumber")      // ← ejecuta con el engine “cucumber”
+@SelectClasspathResource("features")   // rutas *.feature que incluirá
 
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("features")
-@ConfigurationParameter(
+@ConfigurationParameter(         // glue = clases con los steps
         key   = GLUE_PROPERTY_NAME,
-        value = "com.practicando.demo"     // no solo ...steps
-)
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+        value = "com.practicando.demo")
+@ConfigurationParameter(         // plugins de reporte, consola, etc.
+        key   = PLUGIN_PROPERTY_NAME,
+        value = "pretty, html:target/cucumber-reports/report.html, "
+                + "json:target/cucumber-reports/report.json")
+
 public class CucumberTest {
 }
